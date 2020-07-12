9ca9821078e873d746c894a3d9ad8ba5fcb531bf
Onur Bostancıoğlu <onur.bostancioglu@primeapps.io>
2020-07-12 12:04:44 +0300

    Add project files.

---
+++ ApiCore/Data/Context/AppDBContext.cs
@@ -0,0 +1,128 @@
+﻿using ApiCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiCore.Context
+{
+    public class AppDBContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public int? UserId { get; set; }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public AppDBContext(IConfiguration configuration)
+        {
+            Database.GetDbConnection().ConnectionString = configuration.GetConnectionString("DBConnection");
+        }
+
+        public override int SaveChanges()
+        {
+            SetDefaultValues();
+
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            SetDefaultValues();
+
+            return base.SaveChangesAsync();
+        }
+
+        private void SetDefaultValues()
+        {
+            var currentUserId = GetCurrentUserId();
+
+            System.Collections.Generic.IEnumerable<Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry> entities = ChangeTracker.Entries().Where(x => x.Entity is BaseEntity && (x.State == EntityState.Added || x.State == EntityState.Modified));
+
+            foreach (Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry entity in entities)
+            {
+                if (entity.State == EntityState.Added)
+                {
+                    if (((BaseEntity)entity.Entity).CreatedAt == DateTime.MinValue)
+                        ((BaseEntity)entity.Entity).CreatedAt = DateTime.UtcNow;
+
+                    if (((BaseEntity)entity.Entity).CreatedById < 1 && currentUserId > 0)
+                        ((BaseEntity)entity.Entity).CreatedById = currentUserId;
+                }
+                else
+                {
+                    if (!((BaseEntity)entity.Entity).UpdatedAt.HasValue)
+                        ((BaseEntity)entity.Entity).UpdatedAt = DateTime.UtcNow;
+
+                    if (!((BaseEntity)entity.Entity).UpdatedById.HasValue && currentUserId > 0)
+                        ((BaseEntity)entity.Entity).UpdatedById = currentUserId;
+                }
+            }
+        }
+
+        public int GetCurrentUserId()
+        {
+            return UserId ?? 0;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            CreateModelMapping(modelBuilder);
+            modelBuilder.HasDefaultSchema("public");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void CreateModelMapping(ModelBuilder modelBuilder)
+        {
+            //User
+            modelBuilder.Entity<User>().HasIndex(x => x.Email);
+            modelBuilder.Entity<User>().HasIndex(x => x.FullName);
+            modelBuilder.Entity<User>().HasIndex(x => x.CreatedAt);
+            modelBuilder.Entity<User>().HasIndex(x => x.UpdatedAt);
+            modelBuilder.Entity<User>().HasIndex(x => x.Deleted);
+
+            modelBuilder.Entity<Profile>()
+                .HasMany(x => x.Users)
+                .WithOne(x => x.Profile)
+                .HasForeignKey(x => x.ProfileId);
+
+            modelBuilder.Entity<Profile>()
+                .HasOne(x => x.CreatedBy)
+                .WithMany()
+                .HasForeignKey(x => x.CreatedById);
+
+
+            modelBuilder.Entity<Role>()
+                .HasMany(x => x.Users)
+                .WithOne(x => x.Role)
+                .HasForeignKey(x => x.RoleId);
+
+            BuildIndexes(modelBuilder);
+        }
+
+        public void BuildIndexes(ModelBuilder modelBuilder)
+        {
+            //User
+            modelBuilder.Entity<User>().HasIndex(x => x.Email);
+            modelBuilder.Entity<User>().HasIndex(x => x.FullName);
+            modelBuilder.Entity<User>().HasIndex(x => x.CreatedAt);
+            modelBuilder.Entity<User>().HasIndex(x => x.UpdatedAt);
+            modelBuilder.Entity<User>().HasIndex(x => x.Deleted);
+            modelBuilder.Entity<User>().HasIndex(x => new { x.Email }).IsUnique();
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Profile> Profiles { get; set; }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
---
+++ ApiCore/Startup/DIConfig.cs
@@ -0,0 +1,44 @@
+﻿
+using Api_Project.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ApiCore
+{
+    public partial class Startup
+    {
+        public static void DIRegister(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDBContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+
+            services.AddScoped(p => new AppDBContext(p.GetService<DbContextOptions<AppDBContext>>(), configuration));
+
+            //Register all repositories
+            foreach (var assembly in new[] { "Api Project" })
+            {
+                var assemblies = Assembly.Load(assembly);
+                var allServices = assemblies.GetTypes().Where(t => t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract && t.GetTypeInfo().Name.EndsWith("Repository")).ToList();
+
+                foreach (var type in allServices)
+                {
+                    var allInterfaces = type.GetInterfaces().Where(x => x.Name.EndsWith("Repository")).ToList();
+                    var mainInterfaces = allInterfaces.Except(allInterfaces.SelectMany(t => t.GetInterfaces()));
+
+                    foreach (var itype in mainInterfaces)
+                    {
+                        if (allServices.Any(x => x != type && itype.IsAssignableFrom(x)))
+                        {
+                            throw new Exception("The " + itype.Name + " type has more than one implementations, please change your filter");
+                        }
+
+                        services.AddTransient(itype, type);
+                    }
+                }
+            }
+        }
+    }
+}
